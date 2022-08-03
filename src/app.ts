483824a82951d73b37368ea5342b864f7962fbb4
rafaelglz0622 <rafaelglz0622@hotmail.com>
2022-08-03 00:00:03 -0500

    interfaces and stuffs

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,30 @@
+interface IsPerson {
+    name: string,
+    age: number,
+    speak(a: string): void,
+    spend(a: number): number    
+}
+
+const me: IsPerson = {
+    name: "rafael",
+    age: 36,
+    speak(text: string): void {
+        console.log(text);
+    },
+    spend(amount: number): number {
+        return amount;
+    }
+
+}
+
+const greetPerson = (Person: IsPerson) => {
+    console.log('Hello ' , Person.name);
+
+}
+ 
+greetPerson(me);
+
+ 
 import { Invoice } from './classes/invoice.js';
 
 const _invOne = new Invoice('Mario', 'Work on the mario website', 250);
